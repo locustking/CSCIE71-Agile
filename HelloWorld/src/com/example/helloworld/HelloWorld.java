b446d07d905671d468d0c79971895bd763cb80ef
locustking <locustking@users.noreply.github.com>
2016-09-12 14:08:13 -0700

    Add files via upload

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by Daryl Greer on 9/4/2016.
+ * CSCIE71-Agile - Assignment 1:Hello World
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, " + args[0] + "!");
+    }
+
+}
